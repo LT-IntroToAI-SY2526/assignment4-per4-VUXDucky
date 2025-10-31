f7775027fa516895140260ad982329a480c01536
Lukas Leahu <lleahu@cps.edu>
2025-10-31 11:28:11 -0500

    finsihed

--- a4_writeup.md
+++ a4_writeup.md
@@ -5,7 +5,10 @@ In assignment 4 we created a basic tic tac toe game so that we could learn objec
 ## Reflection Questions
 
 1. What was the most difficult part to tic-tac-toe?
+The most difficult part for me in the tic tac toe part was to make the player postion because I had no idea how it should be formatted/ coded.
 
 2. Explain how you would add a computer player to the game.
+A way you could incorporate a computer player to the game is by possily asking it to pick a random number from 0-8 and if it picks a spot already placed it would pick another random number until it picks an open spot. 
 
 3. If you add a computer player, explain (doesn't have to be super technical) how you might get the computer player to play the best move every time. *Note - I am not grading this for a correct answer, I just want to know your thoughts on how you might accomplish it.
+How I think you might is the robot reads the different combinations to win then reads over the positions on the board after the player sets move and then instantly sends it back to the computer and then it keeps going until it wins.
--- notes.py
+++ notes.py
@@ -2,3 +2,58 @@
 
 # (define-struct dog [fur_color name age favorite_food])
 
+class Dog:
+    # A simple dog class to learn OOp concepts
+    # Attributes:
+    #   breed- the breed of the dog
+    #   fur_color- the fur color of the dog
+    #   name - the name of the dog
+    #   age - the age of the dog
+
+
+    def __init__(self, breed = "dog", fur_color = "black", name = "no name", age):
+        """ Intialize a new dog with breed, fur_color, name and age"""
+        self.breed = breed
+        self.fur_color = fur_color
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        """String rep of a dog"""
+        return f"{self.name} is a {self.age} year old {self.fur_color} {self.breed}"
+
+    def bark(self):
+        return f"{self.name} says: Woof, Woof!!"
+
+    def birthday(self):
+        """celebrate the dog's birthday"""
+        self.age += 1
+        print(f"{self.name} is celebrating their {self.age} birthday")
+
+
+    def paint_dog(self, new_color):
+        """Change the fur color of the dog"""
+        old_color = self.fur_color
+        self.fur_color = new_color
+        print(f"{self.name} changed their fur color from {old_color} to {self.fur_color}")
+
+
+
+if __name__ == "__main__":
+    berg_dog = Dog("labrador", "black", "logan", 9)
+    aidan_dog = Dog("lab pitt mix", "grey", "cubbie", 9)
+    default_dog = Dog()
+    matthew_dog = Dog(breed="labrador", name="bella", age=1)
+
+print(berg_dog)
+print(aidan_dog)
+print(default_dog)
+
+print()
+
+print(aidan_dog.bark())
+print(matthew_dog)
+matthew_dog.birthday()
+berg_dog.paint_dog("neon green")
+print()
+print(berg_dog)
